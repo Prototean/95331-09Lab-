d099ff8c5005c254b0cf49f14063a4cff20ee5c3
Prototean <teanchai_ch@cmu.ac.th>
2026-08-31 14:27:14 +0700

    Add maven to project and set the project file

---
+++ src/main/java/se331/lab/controller/EventController.java
@@ -0,0 +1,40 @@
+package se331.lab.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import se331.lab.entity.Event;
+
+import jakarta.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class EventController {
+    List<Event> eventList;
+
+    @PostConstruct
+    public void init() {
+        eventList = new ArrayList<>();
+        eventList.add(Event.builder()
+                .id(123L)
+                .category("animal welfare")
+                .title("Cat Adoption Day")
+                .description("Find your new feline friend at this event.")
+                .location("Meow Town")
+                .date("January 28, 2022")
+                .time("12:00")
+                .petsAllowed(true)
+                .organizer("Kat Laydee")
+                .build());
+        eventList.add(Event.builder()
+                .id(456L)
+                .category("food")
+                .title("Community Gardening")
+                .description("Join us as we tend to the community edible plants.")
+                .location("Flora City")
+                .date("March 14, 2022")
+                .time("10:00")
+                .petsAllowed(true)
+                .organizer("Fern Pollin")
+                .build());
+    }
+}
