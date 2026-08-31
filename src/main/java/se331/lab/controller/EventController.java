19bbf382bdb8ae914821da959a1851d712d392a6
Prototean <teanchai_ch@cmu.ac.th>
2026-08-31 14:57:50 +0700

    Add the event for 6 events Staff check

--- src/main/java/se331/lab/controller/EventController.java
+++ src/main/java/se331/lab/controller/EventController.java
@@ -25,6 +25,7 @@ public class EventController {
                 .petsAllowed(true)
                 .organizer("Kat Laydee")
                 .build());
+
         eventList.add(Event.builder()
                 .id(456L)
                 .category("food")
@@ -35,6 +36,54 @@ public class EventController {
                 .time("10:00")
                 .petsAllowed(true)
                 .organizer("Fern Pollin")
+                .build());
+
+        eventList.add(Event.builder()
+                .id(789L)
+                .category("sustainability")
+                .title("Beach Cleanup")
+                .description("Help pick up trash along the shore.")
+                .location("Playa Del Carmen")
+                .date("July 22, 2022")
+                .time("11:00")
+                .petsAllowed(false)
+                .organizer("Carey Wales")
+                .build());
+
+        eventList.add(Event.builder()
+                .id(1001L)
+                .category("animal welfare")
+                .title("Dog Adoption Day")
+                .description("Find your new canine friend at this event.")
+                .location("Woof Town")
+                .date("August 28, 2022")
+                .time("12:00")
+                .petsAllowed(true)
+                .organizer("Dawng Dahd")
+                .build());
+
+        eventList.add(Event.builder()
+                .id(1002L)
+                .category("food")
+                .title("Canned Food Drive")
+                .description("Bring your canned food to donate to those in need.")
+                .location("Tin City")
+                .date("September 14, 2022")
+                .time("3:00")
+                .petsAllowed(true)
+                .organizer("Kahn Opiner")
+                .build());
+
+        eventList.add(Event.builder()
+                .id(1003L)
+                .category("sustainability")
+                .title("Highway Cleanup")
+                .description("Help pick up trash along the highway.")
+                .location("Highway 50")
+                .date("July 22, 2022")
+                .time("11:00")
+                .petsAllowed(false)
+                .organizer("Broody Kill")
                 .build());
     }
 }
