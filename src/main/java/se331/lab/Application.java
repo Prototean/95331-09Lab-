828e199db0d59f3bba82179404f1aa8c51fdbc76
Prototean <teanchai_ch@cmu.ac.th>
2026-08-31 17:50:55 +0700

    Add exposedHeaders for make the frontend can usable with backend

--- src/main/java/se331/lab/Application.java
+++ src/main/java/se331/lab/Application.java
@@ -23,7 +23,8 @@ public class Application {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173");
+                        .allowedOrigins("http://localhost:5173")
+                        .exposedHeaders("x-total-count");
 
             }
         };
