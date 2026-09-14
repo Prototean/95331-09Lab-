f15eb341152f2e51ff6adb7895e246f0abc80dd7
Prototean <teanchai_ch@cmu.ac.th>
2026-09-14 16:00:10 +0700

    4.8 Add Page to all Event instead List<>

--- src/main/java/se331/lab/controller/EventController.java
+++ src/main/java/se331/lab/controller/EventController.java
@@ -1,6 +1,7 @@
 package se331.lab.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,17 +22,17 @@ public class EventController {
     @GetMapping("events")
     public ResponseEntity<?> getEventLists(@RequestParam(value = "_limit", required = false)Integer perPage
             ,@RequestParam(value = "_page", required = false)Integer page) {
-                List<Event> output = null;
-                Integer eventSize = eventService.getEventSize();
+                Page<Event> pageOutput = eventService.getEvents(perPage, page);
                 HttpHeaders responseHeader = new HttpHeaders();
-                responseHeader.set("x-total-count", String.valueOf(eventSize));
-                try {
-                    output = eventService.getEvents(perPage, page);
-                    return ResponseEntity.ok().headers(responseHeader).body(output);
-                }catch (IndexOutOfBoundsException ex){
-                    return  ResponseEntity.ok().headers(responseHeader).body(output);
+                responseHeader.set("x-total-count",
+                        String.valueOf(pageOutput.getTotalElements()));
+                try{
+                    return
+                            ResponseEntity.ok().headers(responseHeader).body(pageOutput.getContent());
+                } catch(IndexOutOfBoundsException e) {
+                    return
+                            ResponseEntity.ok().headers(responseHeader).body(pageOutput.getContent());
                 }
-
 
     }
 
