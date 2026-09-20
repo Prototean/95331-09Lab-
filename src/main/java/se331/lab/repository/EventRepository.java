33d2dbfb8290e6413e37c4e08e494052bcb421f7
Prototean <teanchai_ch@cmu.ac.th>
2026-09-20 16:41:51 +0700

    Update new one

---
+++ src/main/java/se331/lab/repository/EventRepository.java
@@ -0,0 +1,11 @@
+package se331.lab.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import se331.lab.entity.Event;
+
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<Event, Long> {
+    List<Event> findAll();
+
+}
