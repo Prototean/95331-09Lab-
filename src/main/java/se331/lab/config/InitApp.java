d85a1aeb158fce36471c87b2a2603cbb3259b524
Prototean <teanchai_ch@cmu.ac.th>
2026-09-20 15:29:48 +0700

    Add Organizer page add

--- src/main/java/se331/lab/config/InitApp.java
+++ src/main/java/se331/lab/config/InitApp.java
@@ -5,14 +5,26 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 import se331.lab.entity.Event;
+import se331.lab.entity.Organizer;
 import se331.lab.repository.EventRepository;
+import se331.lab.repository.OrganizerRepository;
 
 @Component
 @RequiredArgsConstructor
 public class InitApp implements ApplicationListener<ApplicationReadyEvent> {
     final EventRepository eventRepostiory;
+    final OrganizerRepository organizerRepository;
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        organizerRepository.save(Organizer.builder()
+                .organizer("CAMT").build());
+        organizerRepository.save(Organizer.builder()
+                .organizer("CMU").build());
+        organizerRepository.save(Organizer.builder()
+                .organizer("Chiang Mai").build());
+        organizerRepository.save(Organizer.builder()
+                .organizer("Chiang Mai Municipality").build());
+
         eventRepostiory.save(Event.builder()
                 .category("Academic")
                 .title("Midterm Exam")
